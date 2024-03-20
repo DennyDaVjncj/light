6e425066db1b42bb7e9a41c470be08260f9c5ffb
DennyDaVjncj <daVjncjCode@gmail.com>
2024-03-20 16:27:22 -0400

    configured the getNext() class

--- lib/main.dart
+++ lib/main.dart
@@ -85,6 +85,10 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
